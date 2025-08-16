d79eb9cd6ffb47f75acd061a4f047b1734677cf8
Dycltl7585 <Dycltl@outlook.com>
2025-08-16 12:35:40 +0800

    Update post.js

--- dist/post.js
+++ dist/post.js
@@ -1,17 +1,20 @@
 import * as core from "@actions/core";
 import * as fs from "fs";
-try {
-    core.info("开始执行 post 清理逻辑...");
-    const tmpFile = process.env.MY_TMP_FILE;
-    if (tmpFile && fs.existsSync(tmpFile)) {
-        fs.unlinkSync(tmpFile);
-        core.info(`已删除临时文件: ${tmpFile}`);
+async function cleanup() {
+    try {
+        core.info("开始执行 post 清理逻辑...");
+        const tmpFile = process.env.MY_TMP_FILE;
+        if (tmpFile && fs.existsSync(tmpFile)) {
+            fs.unlinkSync(tmpFile);
+            core.info(`已删除临时文件: ${tmpFile}`);
+        }
+        else {
+            core.info("没有找到需要清理的临时文件");
+        }
     }
-    else {
-        core.info("没有找到需要清理的临时文件");
+    catch (error) {
+        core.setFailed(error.message);
     }
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+cleanup();
 //# sourceMappingURL=post.js.map
