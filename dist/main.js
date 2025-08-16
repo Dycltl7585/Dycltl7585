f6e2af1f93fe31a78db0553afe440fb5c0fe1f4c
Dycltl7585 <Dycltl@outlook.com>
2025-08-16 12:35:09 +0800

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,14 @@
 import * as core from "@actions/core";
-try {
-    core.info("主逻辑开始执行...");
-    // 模拟一个需要清理的操作，比如设置环境变量
-    core.exportVariable("MY_TMP_FILE", "/tmp/my-action-file.txt");
-    core.info("已设置临时文件路径: /tmp/my-action-file.txt");
+async function run() {
+    try {
+        core.info("主逻辑开始执行...");
+        // 模拟一个需要清理的操作，比如设置环境变量
+        core.exportVariable("MY_TMP_FILE", "/tmp/my-action-file.txt");
+        core.info("已设置临时文件路径: /tmp/my-action-file.txt");
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+run();
 //# sourceMappingURL=main.js.map
