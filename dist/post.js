2215391cccb3560fdfecd3abac6a6edd5a81f7ae
Dycltl7585 <Dycltl@outlook.com>
2025-08-16 11:04:38 +0800

    Create post.js

---
+++ dist/post.js
@@ -0,0 +1,17 @@
+import * as core from "@actions/core";
+import * as fs from "fs";
+try {
+    core.info("开始执行 post 清理逻辑...");
+    const tmpFile = process.env.MY_TMP_FILE;
+    if (tmpFile && fs.existsSync(tmpFile)) {
+        fs.unlinkSync(tmpFile);
+        core.info(`已删除临时文件: ${tmpFile}`);
+    }
+    else {
+        core.info("没有找到需要清理的临时文件");
+    }
+}
+catch (error) {
+    core.setFailed(error.message);
+}
+//# sourceMappingURL=post.js.map
